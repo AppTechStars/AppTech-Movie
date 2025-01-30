03688c680a6c2f254285b6a86e645752d74f26bb
Yayah <yayah0613912091@gmail.com>
2025-01-30 11:54:52 +0200

    colours updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HeroSection from './HeroSection/HeroSection';
 import TrendingMovies from './MoviesFolder/TrendingMovies';
-import Navbar from './NavbarMenu/NavBar';
+import Navbar from './Navbar/NavBar';
 import SearchResults from './MoviesFolder/SearchResults';
 import Footer from './Footer/Footer';
 
